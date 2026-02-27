c8987091312f6775c65f84c0c722012903c14c61
Partho2006 <parthojotyroychowdhury@gmail.com>
2026-02-28 00:12:59 +0530

    Q.153

--- Striver_A2Z/153.Min_in_rotated_sorted_array.java
+++ Striver_A2Z/153.Min_in_rotated_sorted_array.java
@@ -49,3 +49,10 @@ class Solution {
 
 // Time complexity: O(logn) 
 // Space complexity: O(1)
+
+// In a rotated sorted array, the smallest element represents the point of rotation. It is the only element that is smaller than its previous element. Since the array is sorted in two segments, we can use binary search to efficiently find this pivot point. By comparing the middle element with the rightmost element in the current search space, we can determine which half of the array contains the minimum element.
+// Initialize pointers to the start and end of the array.
+// While start is less than end, calculate the middle index.
+// If the middle element is greater than the rightmost element, move the start to mid + 1.
+// Else, move the end to mid (because mid can be the minimum).
+// When the loop ends, start will point to the minimum element.
